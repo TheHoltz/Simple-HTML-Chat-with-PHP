5eddfec69315cdca32add57b831808e1f1268414
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2019-01-02 14:51:57 -0200

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,8 @@
+<!-- Esse arquivo irá servir para realizar as conexões e posteriormente as querys,
+afim de que não seja necessário inserir o trecho repetidamente.
+Utilizando-se apenas include 'db.php';
+-->
+
 <?php
 $host = "HOST";
 $user = "USER";
@@ -6,6 +11,7 @@ $db_name = "chats";
 $con = mysqli_connect($host, $user, $pass, $db_name);
 
 if (!$con) {
+    //Trecho importante para debug
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
